dfd13ebb2d921714cbfbdd5274a1dc50c9879c91
Vybekid <felobitto@gmail.com>
2025-08-03 13:20:10 +0300

    updated

--- captainA.py
+++ captainA.py
@@ -1,22 +1,18 @@
 import turtle
 
-# Set up the screen
 turtle.bgcolor("black")
-turtle.speed(0) # Set speed to fastest
+turtle.speed(3) 
 
-# Function to draw a filled circle
 def draw_circle(radius, color):
     turtle.color(color)
     turtle.begin_fill()
     turtle.circle(radius)
     turtle.end_fill()
 
-# Move to starting position for the first circle
 turtle.penup()
 turtle.goto(0, -300)
 turtle.pendown()
 
-# Draw the concentric circles
 draw_circle(300, "red")
 
 turtle.penup()
@@ -34,23 +30,19 @@ turtle.goto(0, -180)
 turtle.pendown()
 draw_circle(180, "blue")
 
-# Draw the star
 turtle.penup()
-turtle.goto(-170, 65) # Position the turtle to start the star
+turtle.goto(-170, 65)
 turtle.setheading(0)
 turtle.pendown()
 turtle.color("white")
 turtle.begin_fill()
 
-# Loop to draw the 5 points of the star
 for i in range(5):
     turtle.forward(340)
     turtle.right(144)
 
 turtle.end_fill()
 
-# Hide the turtle cursor
 turtle.hideturtle()
 
-# Keep the window open
 turtle.done()
